789b8bea02d703f0936db36d33c8f94c4d8afc06
Abed Halawi <halawi.abed@gmail.com>
2015-01-06 12:38:18 +0200

    implement editor class
    Provides access to all the underlying functionality, acts as a manager.

---
+++ src/Vinelab/Editor/Editor.php
@@ -0,0 +1,54 @@
+<?php namespace Vinelab\Editor;
+
+use View;
+use DOMDocument;
+
+/**
+ * @author Abed Halawi <abed.halawi@vinelab.com>
+ */
+class Editor {
+
+    const INPUT = 'vinelab-editor-text';
+
+    /**
+     * Get the view of the editor.
+     *
+     * @param  string $form The form id in which the content of this editor should exist.
+     * @param  string $content Existing content.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function view($content = null)
+    {
+        return View::make('vinelab.editor::editor')->render();
+    }
+
+    /**
+     * Get the content instance for the given html.
+     *
+     * @param  string $content
+     *
+     * @return \Vinelab\Editor\Content
+     */
+    public function content($content)
+    {
+        return Content::make($content);
+    }
+
+    /**
+     * Get the JSON representation of the content.
+     * Convenience method for calling toJson() on a Content instance.
+     *
+     * @param  string $content
+     * @return StdClass
+     */
+    public function json($content)
+    {
+        return $this->content($content)->toJson();
+    }
+
+    public function input()
+    {
+        return self::INPUT;
+    }
+}
